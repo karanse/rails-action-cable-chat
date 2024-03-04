55af734c90ecfc4dddcf33ea115d3ef747ff8486
sema karan <karan18sema@gmail.com>
2024-03-04 10:40:54 +0100

    complete lesson

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -11,10 +11,19 @@ export default class extends Controller {
       { received: data => this.#insertMessageAndScrollDown(data)}
     )
 
-    #insertMessageAndScrollDown(data) {
-      this.messagesTarget.insertAdjacentHTML("beforeend", data)
-      this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
-    }
-
   }
+  #insertMessageAndScrollDown(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
+
+  resetForm(event) {
+    event.target.reset()
+  }
+
+  disconnect() {
+    console.log("Unsubscribed from the chatroom")
+    this.channel.unsubscribe()
+  }
+
 }
